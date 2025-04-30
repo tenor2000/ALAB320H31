a5a6c50036999b7f66d26cbd2a5f933aebfbffac
tenor2000 <tenor2000@gmail.com>
2025-04-30 15:55:34 -0400

    added some styling and a few interactive buttons

--- src/App.tsx
+++ src/App.tsx
@@ -10,17 +10,38 @@ function App() {
   return (
     <div className="App">
       <h1>Learner Data</h1>
-      <h2>React + TypeScript + Vite</h2>
-      {learnerData.map((learner) => {
-        return (
-          <Learner
-            name={learner.name}
-            bio={learner.bio}
-            scores={learner.scores}
-            key={learner.name}
-          />
-        );
-      })}
+
+      {learnerData.length !== 0 ? (
+        learnerData.map((learner) => {
+          return (
+            <div
+              style={{ border: "1px solid lightgray", borderRadius: "10px" }}
+            >
+              <Learner
+                name={learner.name}
+                bio={learner.bio}
+                scores={learner.scores}
+                key={learner.name}
+              />
+              <div style={{ margin: "1rem" }}>
+                <button
+                  onClick={() =>
+                    setLearnerData(
+                      learnerData.filter(
+                        (learner) => learner.name !== learnerData[0].name
+                      )
+                    )
+                  }
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+          );
+        })
+      ) : (
+        <button onClick={() => setLearnerData([...data])}>Reset</button>
+      )}
     </div>
   );
 }
--- src/components/Learner.tsx
+++ src/components/Learner.tsx
@@ -1,20 +1,39 @@
 import Score from "./Score.tsx";
 import type { LearnerType } from "../assets/learnerData.tsx";
 
+const cardStyle = {
+  padding: "10px",
+  margin: "10px",
+  borderRadius: "10px",
+  maxWidth: "600px",
+};
+
 export default function Learner({ name, bio, scores }: LearnerType) {
   return (
-    <div>
+    <div style={cardStyle}>
       <h2>{name}</h2>
-      <p>{bio}</p>
-      <ul style={{ listStyleType: "none", padding: 0, margin: 0 }}>
-        {scores.map((scoreObj) => (
-          <Score
-            date={scoreObj.date}
-            score={scoreObj.score}
-            key={scoreObj.date + scoreObj.score}
-          />
-        ))}
-      </ul>
+      <div style={{ display: "flex" }}>
+        <div
+          style={{
+            flex: 2,
+            textAlign: "left",
+            paddingLeft: "10px",
+          }}
+        >
+          <p>{bio}</p>
+        </div>
+        <div style={{ flex: 1, textAlign: "left", paddingLeft: "10px" }}>
+          <ul style={{ listStyleType: "none", padding: 0, margin: 0 }}>
+            {scores.map((scoreObj) => (
+              <Score
+                date={scoreObj.date}
+                score={scoreObj.score}
+                key={scoreObj.date + scoreObj.score}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
