702a9386ef32bf81898fa75ebfb6c08f53feb830
tenor2000 <tenor2000@gmail.com>
2025-04-30 13:56:32 -0400

    "initial commit"

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import './App.css'
+
+import Learner from './components/Learner'
+
+function App() {
+  const [learnerData, setLearnerData] = useState({
+    learners: [
+      {
+        name: 'Cait Yomorta',
+        bio: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus placeat nostrum explicabo? Voluptatibus expedita saepe officia optio, commodi totam ratione laudantium ipsum porro molestias, quasi nulla minus vitae laboriosam corrupti Delectus inventore explicabo est odit incidunt rem a recusandae eum pariatur. Aperiam doloremque blanditiis harum voluptate animi fugit beatae asperiores quo, dignissimos sed illum veniam eum accusantium nulla quod voluptatum',
+        scores: [
+          {
+            date: '2018-02-08',
+            score: 77
+          },
+          {
+            date: '2018-04-22',
+            score: 92
+          },
+          {
+            date: '2018-09-15',
+            score: 68
+          }
+        ]
+      },
+      {
+        name: 'Holly Baird',
+        bio: 'Eum molestiae explicabo deserunt, maiores quod eaque omnis tenetur vero ducimus, magnam autem! Quia facere quaerat eum repudiandae dolorum eligendi iure quae. Eos id possimus accusantium, earum animi modi hic.',
+        scores: [
+          {
+            date: '2018-12-14',
+            score: 88
+          },
+          {
+            date: '2019-01-09',
+            score: 79
+        ...
+        // more code below
+}
+
+export default App
